b67b85701f0cab7cec7b7307b9ab7701c9e39f64
GitHub Action <action@github.com>
2023-04-19 13:12:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/allannava/androidviperpackage/services/MyProjectService.kt
+++ src/main/kotlin/com/github/allannava/androidviperpackage/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.allannava.androidviperpackage.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.allannava.androidviperpackage.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
